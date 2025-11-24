9fd07615bcebccd65334d54710e8083dd06c0815
Aref Malekanian <158069533+Aref-Mal@users.noreply.github.com>
2025-11-23 22:07:24 -0500

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import routes from "./routes.js";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
+
+app.use(cors({
+  origin: FRONTEND_URL,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+app.use(express.json());
+app.use('', routes);
+
+export default app;
